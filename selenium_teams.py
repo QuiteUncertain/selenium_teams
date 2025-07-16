de02f3e4d9beb0bfe87cecb3b0aacb57ed05bac8
QuiteUncertain <miguellararichter@hotmail.com>
2025-07-16 15:21:16 -0600

    Update selenium_teams.py

--- selenium_teams.py
+++ selenium_teams.py
@@ -1,269 +1,171 @@
-# from microsoft (e.g. graph.microsoft.com )
-# https://github.com/ritvikkhanna09/AutoLoginBot/blob/master/script.py
+# selenium_teams.py
+
+import os
+import argparse
+import time
+from datetime import datetime
 
 import keyboard
-import selenium
-#from selenium import webdriver
+import undetected_chromedriver as uc
+from dotenv import load_dotenv
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-import os
-from selenium.webdriver.common.keys import Keys
-###########################################################
+from selenium.webdriver.support.ui import WebDriverWait
 
-#enter the link to the website you want to automate login.
+# --- Configuration ---
+# Load credentials and settings from the .env file
+load_dotenv()
+TEAMS_EMAIL = os.getenv("TEAMS_EMAIL")
+TEAMS_PASSWORD = os.getenv("TEAMS_PASSWORD")
+TEAMS_URL = "https://teams.microsoft.com"
 
-
-#enter your login username
-
-#enter your login password
-#to be safe, you'll have to enter your password manually
-#password="corp_guest_wifi_password"
-
-# http://10.255.255.254/logout.html
-
-###########################################################
-
-#enter the element for username input field
-element_for_username="loginfmt"
-#enter the element for password input field
-element_for_password="passwd"
-#enter the element for submit button
-element_for_submit="idSIButton9"
-#enter the element indicating the site works without a captive portal
-teams_works="Microsoft Teams"
-#teams_works="div.teams-title"
-
-###########################################################
+# Using more stable 'data-tid' attributes for selectors where possible
+LOCATORS = {
+    "email_field": (By.ID, "i0116"),
+    "next_button": (By.ID, "idSIButton9"),
+    "password_field": (By.ID, "i0118"),
+    "no_button": (By.ID, "idBtn_Back"),
+    "chat_pane": (By.ID, "chat-pane-list"),
+    "chat_messages_container": (By.CSS_SELECTOR, "div[data-tid='message-pane-list-content']"),
+    "message_group": (By.CSS_SELECTOR, "div[data-tid^='message-']"),
+    "message_author": (By.CSS_SELECTOR, "div[data-tid='message-author']"),
+    "message_timestamp": (By.CSS_SELECTOR, "span.timestamp"),
+    "message_body": (By.CSS_SELECTOR, "div.message-body-content"),
+}
 
 
-def main(outfile="teams.txt", url="https://teams.microsoft.com", username="dietzn@shure.com"):
-         
+def get_webdriver(connect_to_existing: bool) -> uc.Chrome:
+    """Initializes and returns a Selenium WebDriver instance."""
+    options = uc.ChromeOptions()
 
-    existing_browser = True
-    session_url = "http://127.0.0.1:47015"
-    session_id = "4d28c5c4f9aabfd96a9c5b11987363be"
-    
-    if not existing_browser:
-        #browser = webdriver.Safari()    #for macOS users[for others use chrome vis chromedriver]
-        browser = selenium.webdriver.Chrome('/snap/bin/chromium.chromedriver')    #uncomment this line,for chrome users
-    
-        # https://stackoverflow.com/questions/8344776/can-selenium-interact-with-an-existing-browser-session
-        session_url = browser.command_executor._url       #"http://127.0.0.1:60622/hub"
-        session_id = browser.session_id
-        print(session_url)
-        print(session_id)
-        browser.get(url)
+    if connect_to_existing:
+        print("▶️ Attempting to connect to existing Chrome instance on port 9222...")
+        options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
     else:
-        browser = attach_to_session(session_url, session_id)
+        print("🚀 Launching a new Chrome instance...")
 
-    # keyboard event handler
-    def handle_keypress():
-        log_current_chat(browser)
-    
-    #try:
-    # check for a shure captive portal string to see if we need to sign in.
-    # todo: try to handle captive portal (half baked idea... not related to teams scraping directly)
-    
-    time.sleep(1)
-
-#    try:
-#        # see if we need to sign in
-#        captive = False
-#        teams_element = browser.find_element_by_id(teams_works)
-#    except selenium.common.exceptions.NoSuchElementException as e:
-#        print('failed to find teams, try to log in')
-##        captive = True
-#        pass
-    
-    # sign in
-#    if captive:
-        # do this manually
-        #try:
-        #    username_element = browser.find_element_by_name(element_for_username)
-        #    username_element.send_keys(username)        
-        #    signInButton = browser.find_element_by_id(element_for_submit)
-        #    signInButton.click()
-        #    #password_element  = browser.find_element_by_name(element_for_password)
-        #    #password_element.send_keys(password)
-        #except selenium.common.exceptions.NoSuchElementException as e:
-        #    captive = False
-        #    # we're probably already logged in
-        #    pass
-#    else:
-#        time.sleep(1)
-
-    input("press key once logged in to teams and in correct chat")
     try:
-        #teams_element = browser.find_element(By.XPATH, teams_works)
-        if teams_works in browser.page_source:
-            print('success')
-        else:
-            raise ValueError("ack")
-    except selenium.common.exceptions.NoSuchElementException as e:
-        print("giving up, can't reach teams")
-        raise e
-    
-# more stale cruft for loggin in automatically... do manually
-
-#    signInButton = browser.find_element_by_id(element_for_submit)
-#    signInButton.click()
-#    username_element = browser.find_element_by_name("username")
-#    username_element.send_keys(username)
-#    password_element = browser.find_element_by_name("password")
-#    password_element.send_keys("blah")
-#    #click sign in
-#    e = browser.find_element_by_link_text("Call")
-#    # wait for user to input code
-#    #e.click()
-#    #value = Verify
-#    #Stay signed in?
-#    e = browser.find_element_by_id(element_for_submit)
-#    e.click()
-#    #  id = idSIButton9
-#    #signInButton.click() # manually
-#    
-#    #"Stay signed in?"
-#    #click "Yes" id="idSIButton9"
-#    
-#    #todo: check that we're in teams
-
-    # register global hotkey to start teams log
-    # must be root on linux
-    # todo: find another method
-    #keyboard.add_hotkey('ctrl+shift+t', handle_keypress)
-                        
-    # # send ctrl-shift-2 to go to messages
-    # # https://stackoverflow.com/questions/45902295/how-to-send-shortcut-keys-using-selenium-in-python-to-move-through-webpage
-    # browser.find_element_by_tag_name('body').send_keys(Keys.CONTRL + Keys.SHIFT + '2')
-    # # might need to press up a bunch of times to get to the top of the chat list
-    # # select first chat
-    # 
-    # e = browser.switch_to.active_element
-    # e.send_keys(Keys.ENTER)
-
-    #keyboard.remove_all_hotkeys()
-    log_current_chat(browser, outfile)
-    #browser.quit()
-    
-
-    
-def log_current_chat(browser, outfile):
-    # start logging now
-    # trigger this with a hotkey
-    
-    # select newest chat text
-    e = browser.switch_to.active_element
-    e.send_keys(Keys.SHIFT + Keys.TAB)
-    # https://stackoverflow.com/questions/44834358/switch-to-an-iframe-through-selenium-and-python
-    #browser.switch_to.frame(e)
-
-    dump_complete = False
-    prev_s = ""
-    with open(outfile, 'a') as f:
-        duplicate_count = 0
-        while not dump_complete:
-            # grab next element text
-            e = browser.switch_to.active_element
-            # grab string
-            s = e.text
-            #s = e.get_attribute('innerHTML')
-            #s = e.get_attribute('innertext')
-            print(e)
-            print(s)
-            e.send_keys(Keys.UP)
-            try:
-                browser.switch_to.frame(e)
-            except:
-                pass
-            if s == prev_s:
-                duplicate_count +=1
-            else:
-                duplicate_count = 0
-            prev_s = s
-            # if we see a lot of duplicates, we're probably at the top, so give up
-            if duplicate_count > 10:
-                dump_complete = True
-            print(s, file=f)
-    
-
-# https://stackoverflow.com/questions/8344776/can-selenium-interact-with-an-existing-browser-session
-from selenium import webdriver
-from selenium.webdriver.remote.webdriver import WebDriver
-
-# executor_url = driver.command_executor._url
-# session_id = driver.session_id
-
-def attach_to_session(executor_url, session_id):
-    original_execute = WebDriver.execute
-    def new_command_execute(self, command, params=None):
-        if command == "newSession":
-            # Mock the response
-            return {'success': 0, 'value': None, 'sessionId': session_id}
-        else:
-            return original_execute(self, command, params)
-    # Patch the function before creating the driver object
-    WebDriver.execute = new_command_execute
-    driver = webdriver.Remote(command_executor=executor_url, desired_capabilities={})
-    driver.session_id = session_id
-    # Replace the patched function with original function
-    WebDriver.execute = original_execute
+        driver = uc.Chrome(options=options, version_main=108)
+        driver.implicitly_wait(5) # Set a base implicit wait
+    except Exception as e:
+        print(f"❌ Error initializing WebDriver: {e}")
+        print("Ensure Chrome is running with '--remote-debugging-port=9222' if using --connect.")
+        return None
+        
     return driver
 
 
-# https://stackoverflow.com/questions/28889401/detect-user-key-mouse-in-python-selenium
-#boppreh/keyboard
-    
-    
-    #e = browser.switch_to.active_element; e.text
-    #"11:36 PM\nI don't. Hold tight. It's not midnight yet. I said today. :-) Why are you up so late?"
-    
-    
-    #e = browser.switch_to.active_element
-    #e.text
-    
-    #arrow up to go back
-    #e.text has message content
-    #div id messageBody
-    #name:
-    #div class ts-msg-name app-small-font
-    #timestamp:
-    #div  div div div class timestamp-column span class ts-created message-datetime
-    # save name, date, text, hash?
-    # if name, date, text
-    
-    #if this hash is not in the hash table
-    #    add this to the log and to the hash table
-    #if it is in the hash table, we are done
-    # unless there is a flag to go farther back (to scrape to the beginning of time, or some other limit)
-    
-    # determine if we are at the beginning of all messages (got same message over and over, pressing up?)
-    
-    
-    #"repeat for next chat (unique group of users)"
-    #"repeat for groups as well
-    
-    
-    # then run ssl
-    # ssl()
-    
-    
-        #### to quit the browser uncomment the following lines ####
-        # time.sleep(3)
-        # browser.quit()
-        # time.sleep(1)
-        # browserExe = "Safari"
-        # os.system("pkill "+browserExe)
-    #except Exception:
-        #### This exception occurs if the element are not found in the webpage.
-    #    print("Some error occured :(")
-    
-        #### to quit the browser uncomment the following lines ####
-        # browser.quit()
-        # browserExe = "Safari"
-        # os.system("pkill "+browserExe)
+def login(driver: uc.Chrome):
+    """Handles the login process for Microsoft Teams."""
+    try:
+        print("🔐 Starting login process...")
+        wait = WebDriverWait(driver, 20)
 
-if __name__ == '__main__':
+        # Enter email
+        email_field = wait.until(EC.visibility_of_element_located(LOCATORS["email_field"]))
+        email_field.send_keys(TEAMS_EMAIL)
+        driver.find_element(*LOCATORS["next_button"]).click()
+        print("    -> Email submitted.")
+
+        # Enter password
+        password_field = wait.until(EC.visibility_of_element_located(LOCATORS["password_field"]))
+        password_field.send_keys(TEAMS_PASSWORD)
+        driver.find_element(*LOCATORS["next_button"]).click()
+        print("    -> Password submitted.")
+        
+        # Handle "Stay signed in?" prompt
+        no_button = wait.until(EC.visibility_of_element_located(LOCATORS["no_button"]))
+        no_button.click()
+        print("    -> Handled 'Stay signed in?' prompt.")
+        
+        # Wait for the main chat interface to load
+        wait.until(EC.visibility_of_element_located(LOCATORS["chat_pane"]))
+        print("✅ Login successful!")
+
+    except Exception as e:
+        print(f"❌ Login failed. The page structure may have changed. Error: {e}")
+
+
+def scrape_current_chat(driver: uc.Chrome):
+    """Scrapes all messages from the currently open chat pane."""
+    print("\n scraping chat content...")
+    wait = WebDriverWait(driver, 10)
+    
+    try:
+        # Wait for the message container to be present
+        message_container = wait.until(EC.presence_of_element_located(LOCATORS["chat_messages_container"]))
+        
+        # Find all message groups within the container
+        message_groups = message_container.find_elements(*LOCATORS["message_group"])
+        
+        if not message_groups:
+            print("No messages found in the current chat.")
+            return
+
+        # Prepare filename
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        filename = f"teams_chat_{timestamp}.txt"
+        
+        scraped_count = 0
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(f"Scraped from: {driver.title}\n")
+            f.write(f"Timestamp: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+            f.write("-" * 40 + "\n\n")
+
+            for group in message_groups:
+                try:
+                    # Extract author, timestamp, and message body from each message
+                    author = group.find_element(*LOCATORS["message_author"]).text
+                    timestamp = group.find_element(*LOCATORS["message_timestamp"]).get_attribute('title')
+                    body = group.find_element(*LOCATORS["message_body"]).text
+                    
+                    f.write(f"[{timestamp}] {author}:\n")
+                    f.write(f"{body}\n")
+                    f.write("-" * 20 + "\n")
+                    scraped_count += 1
+                except Exception:
+                    # Skip elements that aren't full messages (e.g., "You joined the chat")
+                    continue
+        
+        print(f"✅ Success! Scraped {scraped_count} messages to '{filename}'")
+
+    except Exception as e:
+        print(f"❌ Failed to scrape chat. Could not find message elements. Error: {e}")
+
+
+def main():
+    """Main function to run the script."""
+    parser = argparse.ArgumentParser(description="Scrape Microsoft Teams chat content.")
+    parser.add_argument(
+        "--connect",
+        action="store_true",
+        help="Connect to an existing Chrome session instead of launching a new one."
+    )
+    args = parser.parse_args()
+
+    driver = get_webdriver(connect_to_existing=args.connect)
+    if not driver:
+        return
+        
+    if not args.connect:
+        driver.get(TEAMS_URL)
+        login(driver)
+    
+    print("\n" + "="*50)
+    print("✅ Browser is ready. Navigate to the desired chat.")
+    print("   Press the ENTER key in this terminal to start scraping.")
+    print("="*50)
+
+    # Wait for the user to press Enter before proceeding
+    keyboard.wait("enter")
+    
+    scrape_current_chat(driver)
+    
+    # Keep the browser open for inspection after script finishes
+    print("\nScript finished. Browser will remain open. Close it manually.")
+    # uncomment the line below to automatically close the browser
+    # driver.quit()
+
+
+if __name__ == "__main__":
     main()
-
